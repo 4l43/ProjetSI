a9976a5dee06b4b56b3b06aa70c16e9c3d38713a
CPLilith <148537575+CPLilith@users.noreply.github.com>
2024-11-21 12:20:21 +0100

    Delete mySiteWeb/App1 directory

--- mySiteWeb/App1/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-# Create your views here.
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the App1 index")
